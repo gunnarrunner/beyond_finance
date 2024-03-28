92d75bb8f81ff0ddf21623cda9db952e3aae02f1
Gunnar <dunkcrunk@gmail.com>
2024-03-27 20:30:54 -0600

    tried putting this in the git ignore file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :tradelines
+  namespace :api do
+    namespace :v1 do
+      resources :tradelines, only: [:index, :show] do
+        resources :deposits, only: :create
+      end
+    end
+  end
 end
